fdb708a73326944396a2a0e4573e66b42e60fddc
Akos Becsey <becsey.a@gmail.com>
2021-12-06 23:05:51 +0100

    Day 1 - Sonar sweep

--- 2021/day01/main.cpp
+++ 2021/day01/main.cpp
@@ -2,8 +2,21 @@
 
 int main()
 {
+    int prev;
+    int count = 0;
+
     forEachLine([&](istringstream& line, int lineIdx) -> void {
         //
-        cout << line.str() << '\n';
+
+        int val = getVariable<int>(line);
+
+        if (lineIdx > 0) {
+            if (val > prev)
+                count++;
+        }
+
+        prev = val;
     });
+
+    cout << "Increased " << count << " times\n";
 }
