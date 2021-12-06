1d8e2b1ee898d5fdee08ce61a47ec5c374ae8149
Akos Becsey <becsey.a@gmail.com>
2021-12-06 23:05:51 +0100

    Day 1 - Sonar sweep **

--- 2021/day01/main.cpp
+++ 2021/day01/main.cpp
@@ -2,20 +2,28 @@
 
 int main()
 {
-    int prev;
-    int count = 0;
+    constexpr int WINDOW_SIZE = 3;
+
+    int         count = 0;
+    vector<int> window(WINDOW_SIZE, 0);
 
     forEachLine([&](istringstream& line, int lineIdx) -> void {
         //
 
         int val = getVariable<int>(line);
 
-        if (lineIdx > 0) {
-            if (val > prev)
+        if (lineIdx >= WINDOW_SIZE) {
+            int oldSum = 0;
+            for (auto& element : window)
+                oldSum += element;
+
+            int newSum = oldSum - window[lineIdx % WINDOW_SIZE] + val;
+
+            if (newSum > oldSum)
                 count++;
         }
 
-        prev = val;
+        window[lineIdx % WINDOW_SIZE] = val;
     });
 
     cout << "Increased " << count << " times\n";
