a88ceb71bae2f1d2c99ff24be9de8a9b0fa8f781
Akos Becsey <becsey.a@gmail.com>
2021-12-07 09:02:20 +0100

    Day 3 - Binary Diagnostic **

--- 2021/day03/main.cpp
+++ 2021/day03/main.cpp
@@ -1,40 +1,83 @@
 #include "../../common.hpp"
 
+enum class DominantBit
+{
+    One,
+    Zero,
+    Ambiguous,
+};
+
+DominantBit getDominantBitAtPosition(list<string>& numbers, unsigned pos)
+{
+    int sum = 0;
+
+    for (auto const& numberString : numbers) {
+        if (numberString[pos] == '1')
+            sum++;
+        else if (numberString[pos] == '0')
+            sum--;
+        else
+            throw runtime_error("String is not binary");
+    }
+
+    if (sum > 0)
+        return DominantBit::One;
+    else if (sum < 0)
+        return DominantBit::Zero;
+    else
+        return DominantBit::Ambiguous;
+}
+
+void removeFromListIfCharacterDiffers(list<string>& strings, unsigned pos, char c)
+{
+    auto it = strings.begin();
+
+    while (it != strings.end()) {
+        if (it->at(pos) != c)
+            it = strings.erase(it);
+        else
+            it++;
+    }
+}
+
+string reduceListToSingle(list<string> numbers, bool normalPolarity)
+{
+    unsigned bits = numbers.begin()->size();
+
+    char cOne  = normalPolarity ? '1' : '0';
+    char cZero = normalPolarity ? '0' : '1';
+
+    for (int pos = 0; pos < bits; pos++) {
+        auto dominant = getDominantBitAtPosition(numbers, pos);
+
+        if (dominant == DominantBit::One)
+            removeFromListIfCharacterDiffers(numbers, pos, cOne);
+        else if (dominant == DominantBit::Zero)
+            removeFromListIfCharacterDiffers(numbers, pos, cZero);
+        else if (dominant == DominantBit::Ambiguous)
+            removeFromListIfCharacterDiffers(numbers, pos, cOne);
+
+        if (numbers.size() == 1)
+            return numbers.back();
+    }
+
+    throw runtime_error("Not found");
+}
+
 int main()
 {
-    vector<int> sums;
+    list<string> numbers;
 
     forEachLine([&](istringstream& line, int lineIdx) -> void {
         //
 
-        string binaryStr = line.str();
-
-        for (int i = 0; i < binaryStr.length(); i++) {
-            // fill vector at first line
-            if ((lineIdx == 0) && (i >= sums.size()))
-                sums.push_back(0);
-
-            // keep track of ones and zeros
-            if (binaryStr[i] == '1')
-                sums[i]++;
-            else if (binaryStr[i] == '0')
-                sums[i]--;
-            else
-                throw runtime_error("String is not binary");
-        }
+        numbers.push_back(line.str());
     });
 
-    unsigned bits  = sums.size();
-    unsigned gamma = 0;
+    string strOxygen = reduceListToSingle(numbers, true);
+    string strCO2    = reduceListToSingle(numbers, false);
 
-    for (int i = 0; i < bits; i++) {
-        if (sums[i] > 0)
-            gamma |= (1 << (bits - 1 - i));
-        else if (sums[i] == 0)
-            throw runtime_error("Ambiguous");
-    }
+    auto multiplied = stoull(strOxygen, 0, 2) * stoull(strCO2, 0, 2);
 
-    unsigned epsilon = gamma ^ ((1 << sums.size()) - 1);
-
-    cout << gamma * epsilon << "\n";
+    cout << multiplied << "\n";
 }
