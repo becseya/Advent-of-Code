87a396d6884b18f8967bcdb22b5f3f26c0b3a7ea
Akos Becsey <becsey.a@gmail.com>
2021-12-08 18:46:10 +0100

    Day 5 - Hydrothermal Venture **

--- 2021/day05/main.cpp
+++ 2021/day05/main.cpp
@@ -25,13 +25,21 @@ PointPair parseLine(istringstream& line)
 
 using map_t = vector<vector<int>>;
 
-void swapIfReversed(int& val1, int& val2)
+void swap(int& val1, int& val2)
+{
+    int temp = val1;
+    val1     = val2;
+    val2     = temp;
+}
+
+bool swapIfReversed(int& val1, int& val2)
 {
     if (val2 < val1) {
-        int temp = val1;
-        val1     = val2;
-        val2     = temp;
+        swap(val1, val2);
+        return true;
     }
+
+    return false;
 }
 
 void drawLine(map_t& map, PointPair entry)
@@ -44,6 +52,18 @@ void drawLine(map_t& map, PointPair entry)
         swapIfReversed(entry.y0, entry.y1);
         for (int y = entry.y0; y <= entry.y1; y++)
             map[entry.x0][y]++;
+    } else {
+
+        if (swapIfReversed(entry.x0, entry.x1))
+            swap(entry.y0, entry.y1);
+
+        int y  = entry.y0;
+        int dy = entry.y1 > y ? 1 : -1;
+
+        for (int x = entry.x0; x <= entry.x1; x++) {
+            map[x][y]++;
+            y += dy;
+        }
     }
 }
 
