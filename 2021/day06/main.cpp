013d6625ec099498d4d6985e7915587929b90cc8
Akos Becsey <becsey.a@gmail.com>
2021-12-09 00:13:29 +0100

    Day 6 - Lanternfish **

--- 2021/day06/main.cpp
+++ 2021/day06/main.cpp
@@ -1,65 +1,70 @@
 #include "../../common.hpp"
 
-struct Fish
+struct FishInventory
 {
     static constexpr unsigned REPRODUCE_AFTER_BIRTH = 8;
     static constexpr unsigned REPRODUCE_NORMAL      = 6;
 
-    unsigned daysToRepoduce;
+    using fish_count_t = unsigned long long;
 
-    Fish(unsigned daysToRepoduce)
-        : daysToRepoduce(daysToRepoduce)
+    vector<fish_count_t> inventory;
+
+    FishInventory()
+        : inventory(REPRODUCE_AFTER_BIRTH + 1, 0)
     {}
 
-    Fish()
-        : daysToRepoduce(REPRODUCE_AFTER_BIRTH)
-    {}
+    void addInitial(unsigned daysToRepoduce)
+    { //
+        inventory[daysToRepoduce]++;
+    }
 
-    bool update()
+    void update()
     {
-        if (daysToRepoduce == 0) {
-            daysToRepoduce = REPRODUCE_NORMAL;
-            return true;
-        } else {
-            daysToRepoduce--;
-            return false;
-        }
+        fish_count_t numOfParents = inventory[0];
+
+        for (int i = 0; i < inventory.size() - 1; i++)
+            inventory[i] = inventory[i + 1];
+
+        inventory[REPRODUCE_NORMAL] += numOfParents;
+        inventory[REPRODUCE_AFTER_BIRTH] = numOfParents;
+    }
+
+    fish_count_t getTotalNumber()
+    {
+        fish_count_t sum = 0;
+
+        for (auto& n : inventory)
+            sum += n;
+
+        return sum;
     }
 };
 
-void parseInitialFishes(vector<Fish>& fishes, istringstream& line)
+void parseInitialFishes(FishInventory& inventory, istringstream& line)
 {
     unsigned n;
 
     while (line >> n) {
-        fishes.push_back(Fish(n));
+        inventory.addInitial(n);
         line.ignore(1); // skip the comma
     }
 }
 
 int main()
 {
-    constexpr unsigned DAYS = 80;
+    constexpr unsigned DAYS = 256;
 
-    vector<Fish> fishes;
+    FishInventory inventory;
 
     forEachLine([&](istringstream& line, int lineIdx) -> void {
         //
-        parseInitialFishes(fishes, line);
+        parseInitialFishes(inventory, line);
     });
 
-    for (unsigned i = 0; i < DAYS; i++) {
-        unsigned newBorns = 0;
+    for (unsigned i = 0; i < DAYS; i++)
+        inventory.update();
 
-        for (auto& fish : fishes) {
-            if (fish.update())
-                newBorns++;
-        }
-
-        fishes.insert(fishes.end(), newBorns, Fish());
-    }
-
-    cout << fishes.size() << '\n';
+    cout << inventory.getTotalNumber() << '\n';
 
     return 0;
 }
